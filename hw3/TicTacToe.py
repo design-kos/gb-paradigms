bb46896914b68581110aa1e4b6a81b4e05f67ba2
design-kos <109647043+design-kos@users.noreply.github.com>
2023-09-29 10:37:08 +0500

    Update TicTacToe.py

--- hw3/TicTacToe.py
+++ hw3/TicTacToe.py
@@ -1,7 +1,6 @@
 class TicTacToe:
 
     def __init__(self) -> None:
-        """Инициализация игровой доски и текущего игрока"""
         self.board = [[' ' for _ in range(3)] for _ in range(3)]
         self.current_player = 'X'
 
